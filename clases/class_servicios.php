0cf90420f6b73c7ebbdc05b41d80acf91e7dc07c
Marco Gonzalez <komar.gonzalez@gmail.com>
2025-09-17 22:50:40 -0600

    Update class_servicios.php

--- clases/class_servicios.php
+++ clases/class_servicios.php
@@ -429,7 +429,7 @@
                             'description' => 'Cargo inicial para suscripción',
                             'device_session_id' => $device_session_id,
                             'use_3d_secure' => true,
-                            'redirect_url' => 'code4you://callback'
+                            'redirect_url' => 'https://codeforyou.com.mx/dev/app_code4you/openpay.php'
                         ];
 
                         $charge = $customer->charges->create($chargeRequest);
